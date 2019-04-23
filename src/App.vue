15b799e8c9d24e561ecb8ef89dd528844612f7c7
xiejianxin520 <1411778970@qq.com>
2019-04-23 23:39:47 +0800

    axios优化完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 从页面内存加载路由界面，无需再渲染 -->
+    <keep-alive>
+      <router-view />
+    </keep-alive>
+
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommed from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
   name: 'home',
@@ -31,12 +32,17 @@ export default {
       swiperList: [],
       iconList: [],
       recommendList: [],
-      weekendList: []
+      weekendList: [],
+      lastcity: ''
     }
+  },
+  computed: {
+    ...mapState(['city']) //这个city是vuex中的，是跟其他组件变化而变化
   },
   methods: {
     getHomeInfo() {
-      axios.get('/static/mock/index.json').then(res => {
+      //把当前其他组件练习的city传给get请求
+      axios.get('/static/mock/index.json?city=' + this.city).then(res => {
         //要用箭头函数，不然axios中this是不存在的
         // console.log(res.data.data)
         const reluct = res.data
@@ -50,7 +56,17 @@ export default {
     }
   },
   mounted() {
+    console.log('设置了keepliev我就不会一直渲染了')
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated() {
+    //页面重新加载执行
+    if (this.lastCity !== this.city) {
+      //如果上次城市跟这次城市不一样，就要重新获取AJAX数据
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
