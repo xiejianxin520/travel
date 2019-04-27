786050eb9ab51a6221eb3ae6adb15388d8729a25
xiejianxin520 <1411778970@qq.com>
2019-04-27 17:38:13 +0800

    detail-city fished

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,18 +2,45 @@
   <div>
     <banner></banner>
     <detail-header></detail-header>
-    <div class="strend"></div>
+    <div class="strend">
+      <detail-city :banlist='list'></detail-city>
+    </div>
   </div>
 </template>
 
 <script>
 import banner from './components/banner'
 import DetailHeader from './components/DetailHeader'
+import DetailCity from './components/DetailCity'
 export default {
   name: 'detail',
   components: {
     banner,
-    DetailHeader
+    DetailHeader,
+    DetailCity
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人五人票'
+            },
+            {
+              title: '成人特惠超级十人票'
+            }
+          ]
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '学生票'
+        }
+      ]
+    }
   }
 }
 </script>
